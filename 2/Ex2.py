2a5545c761ddb8b7fd49980a6701edfa861c01b8
JuliaMaria <j.may2501@gmail.com>
2019-03-12 15:59:03 +0100

    Add files via upload

---
+++ 2/Ex2.py
@@ -0,0 +1,23 @@
+import numpy as np
+
+def rank(subset, n): 
+
+    result = np.repeat(0, n)
+
+    for x in range(n):
+        if x+1 in subset:
+            result[x] = 1
+    print result
+    rank = 0  
+    for x in range(n-1,-1,-1):
+        power = n-(x+1)
+        rank = rank + result[x]*np.power(2, power)
+    print rank
+
+rank([], 3)
+rank([1], 3)
+rank([1, 2], 3)
+rank([1, 2, 3], 3)
+
+
+
