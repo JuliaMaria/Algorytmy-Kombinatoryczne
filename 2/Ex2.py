eee09a4f478b37d1d8b2b3baf07408487ca3307d
JuliaMaria <j.may2501@gmail.com>
2019-03-12 15:57:00 +0100

    Add files via upload

--- 2/Ex2.py
+++ 2/Ex2.py
@@ -1,21 +1,21 @@
 import numpy as np
 
-def rank(subset, n): 
+def unrank(r, n): 
 
-    result = np.repeat(0, n)
+    binary = format(r, "b")
+    while len(binary) < n:
+        binary = str(0)+binary
+    binary = [int(i) for i in binary]
+    result = []
+    for (idx, value) in enumerate(binary):
+        if value == 1:
+            result.append(idx+1)
+    print result
+    
 
-    for x in range(n):
-        if x+1 in subset:
-            result[x] = 1
-    rank = 0  
-    for x in range(n-1,-1,-1):
-        power = n-(x+1)
-        rank = rank + result[x]*np.power(2, power)
-    print rank
-
-rank([], 3)
-rank([1], 3)
-rank([1, 2], 3)
-rank([1, 2, 3], 3)
+unrank(1, 3)
+unrank(2, 3)
+unrank(3, 3)
+unrank(4, 3)
 
 
